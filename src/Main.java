120d76b7281b03c3f4916854a7c26d80ab657f53
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-24 12:03:59 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,26 @@ public class Main {
         System.out.println("Udemy: Java Programming Masterclass");
         System.out.println("Coding Exercise: Decimal Format");
 
-        /*System.out.println(areEqualByThreeDecimalPlaces(3.1756, 3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.1756, 3.175));
         System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
         System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
-        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));*/
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
 
-
-        DecimalFormat dfSharp = new DecimalFormat("#.###");
+/*      DecimalFormat dfSharp = new DecimalFormat("#.###");
         dfSharp.setRoundingMode(RoundingMode.FLOOR);
-        System.out.println("3.1756 = " + dfSharp.format(3.1756));
-
-
+        System.out.println("3.1756 = " + dfSharp.format(3.1756));*/
     }
 
     public static boolean areEqualByThreeDecimalPlaces(double firstValue, double secondValue) {
         DecimalFormat df = new DecimalFormat();
-        df.setMaximumFractionDigits(3);
-        String firstValueString = df.format(firstValue);
-        String secondValueString = df.format(secondValue);
-        System.out.printf("firstValueString " + firstValueString + "  secondValueString " + secondValueString + "  ");
+        df.setRoundingMode(RoundingMode.FLOOR);
+        System.out.println("firstValue= " + firstValue + ", secondValue = " + secondValue);
+        System.out.println("df-firstValue= " + df.format(firstValue) + ", df-secondValue = " + df.format(secondValue));
 
-
-//        if (df.format(firstValue) == df.format(secondValue)) {
-//            return true;
-//        } else {
+        if (df.format(firstValue) == df.format(secondValue)) {
+            return true;
+        } else {
             return false;
-        //}
+        }
     }
 }
